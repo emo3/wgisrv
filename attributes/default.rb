eb616faf7b3eec4f89063d14f3b75b6d02772465
Ed Overton <e.overton@gmail.com>
2018-05-07 19:22:15 -0400

    work on install

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,13 @@
-default['wgisrv']['cots_dir']    = '/sfcots'
-default['wgisrv']['app_dir']     = "#{node['wgisrv']['cots_dir']}/apps"
-default['wgisrv']['ng_dir']      = "#{node['wgisrv']['app_dir']}/netcool"
-default['wgisrv']['in_dir']      = "#{node['wgisrv']['app_dir']}/install"
-default['wgisrv']['temp_dir']    = '/tmp'
-default['wgisrv']['media_url']   = 'http://10.1.1.30/media'
-default['wgisrv']['nc_act']      = 'netcool'
-default['wgisrv']['nc_grp']      = 'ncoadmin'
-default['wgisrv']['root_pwd']    = 'nc0Adm1n'
-default['wgisrv']['pa_epwd']     = '@44:GlEwL3cFaU+Pfjcm5S7xNs00PubYXnWhTqEVDtgvUjo=@'
-default['wgisrv']['encryption']  = 'AES'
+default['wgisrv']['cots_dir']  = '/sfcots'
+default['wgisrv']['app_dir']   = "#{node['wgisrv']['cots_dir']}/apps"
+default['wgisrv']['ng_dir']    = "#{node['wgisrv']['app_dir']}/netcool"
+default['wgisrv']['in_dir']    = "#{node['wgisrv']['app_dir']}/install"
+default['wgisrv']['temp_dir']  = '/tmp'
+default['wgisrv']['media_url'] = 'http://10.1.1.30/media'
+default['wgisrv']['nc_act']    = 'netcool'
+default['wgisrv']['nc_grp']    = 'ncoadmin'
+default['wgisrv']['was_epwd']  = 'AjEXH\/Rc1xpV0MEsp3EhHQ=='
+default['wgisrv']['dash_pwd']  = 'c@tch-22'
 # Taken from environmental variables via .kitchen.yml
-default['PAP']                   = ''
-default['PA']                    = ''
+default['PAP']                 = ''
+default['PA']                  = ''
--- recipes/make_nc_wg.rb
+++ recipes/make_nc_wg.rb
@@ -62,3 +62,29 @@ filelist.each do |_fn, fv|
     action :delete
   end
 end
+
+# create silent install file for WebGUI packages
+template "#{node['wgisrv']['temp_dir']}/wg.xml" do
+  source 'wg.xml.erb'
+  sensitive true
+  mode 0444
+end
+
+# Install WebGUI
+execute "install_wg" do
+  command "#{node['wgisrv']['app_dir']}/InstallationManager/eclipse/tools/imcl \
+  input #{node['wgisrv']['temp_dir']}/wg.xml \
+  -log #{node['wgisrv']['temp_dir']}/wg_log.xml \
+  -acceptlicense"
+  cwd "#{node['wgisrv']['app_dir']}/InstallationManager/eclipse/tools"
+  # not_if { File.exist?("#{fv['ipath']}/#{fv['ifile']}") }
+  user node['wgisrv']['nc_act']
+  group node['wgisrv']['nc_grp']
+  umask '022'
+  action :run
+end
+
+# remove silent install file
+file "#{node['wgisrv']['temp_dir']}/wg.xml" do
+  action :nothing
+end
