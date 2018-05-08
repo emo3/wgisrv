6504216862b81a8a1559e4cc63a4256a654f3f50
Ed Overton <e.overton@gmail.com>
2018-05-08 16:33:20 -0400

    more work on template

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,13 +2,12 @@ default['wgisrv']['cots_dir']  = '/sfcots'
 default['wgisrv']['app_dir']   = "#{node['wgisrv']['cots_dir']}/apps"
 default['wgisrv']['ng_dir']    = "#{node['wgisrv']['app_dir']}/netcool"
 default['wgisrv']['in_dir']    = "#{node['wgisrv']['app_dir']}/install"
+default['wgisrv']['shr_dir']   = "#{node['wgisrv']['app_dir']}/IMShared"
 default['wgisrv']['was_dir']   = "#{node['wgisrv']['app_dir']}/WebSphere/AppServer"
+default['wgisrv']['jaz_dir']   = "#{node['wgisrv']['app_dir']}/JazzSM"
 default['wgisrv']['temp_dir']  = '/tmp'
 default['wgisrv']['media_url'] = 'http://10.1.1.30/media'
 default['wgisrv']['nc_act']    = 'netcool'
 default['wgisrv']['nc_grp']    = 'ncoadmin'
-default['wgisrv']['was_epwd']  = 'AjEXH\/Rc1xpV0MEsp3EhHQ=='
+default['wgisrv']['was_epwd']  = 'AjEXH/Rc1xpV0MEsp3EhHQ=='
 default['wgisrv']['dash_pwd']  = 'c@tch-22'
-# Taken from environmental variables via .kitchen.yml
-default['PAP']                 = ''
-default['PA']                  = ''
--- recipes/make_nc_wg.rb
+++ recipes/make_nc_wg.rb
@@ -68,6 +68,7 @@ template "#{node['wgisrv']['temp_dir']}/was.xml" do
   source 'was.xml.erb'
   not_if { File.exist?("#{node['wgisrv']['was_dir']}/startServer.sh") }
   mode 0444
+  action :nothing
 end
 
 # Install was + fp + java 8
@@ -81,10 +82,36 @@ execute 'install_was' do
   user node['wgisrv']['nc_act']
   group node['wgisrv']['nc_grp']
   umask '022'
-  action :run
+  action :nothing
 end
 
 # remove silent install file
 file "#{node['wgisrv']['temp_dir']}/was.xml" do
+  action :delete
+end
+
+# create silent install file for WebGUI packages
+template "#{node['wgisrv']['temp_dir']}/wg.xml" do
+  source 'wg.xml.erb'
+  not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/bin/runcgi") }
+  mode 0444
+end
+
+# Install webgui
+execute 'install_wg' do
+  command "#{node['wgisrv']['app_dir']}/InstallationManager/eclipse/tools/imcl \
+  input #{node['wgisrv']['temp_dir']}/wg.xml \
+  -log #{node['wgisrv']['temp_dir']}/wg_log.xml \
+  -acceptlicense"
+  cwd "#{node['wgisrv']['app_dir']}/InstallationManager/eclipse/tools"
+  not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/bin/runcgi") }
+  user node['wgisrv']['nc_act']
+  group node['wgisrv']['nc_grp']
+  umask '022'
+  action :run
+end
+
+# remove silent install file
+file "#{node['wgisrv']['temp_dir']}/wg.xml" do
   action :delete
 end
