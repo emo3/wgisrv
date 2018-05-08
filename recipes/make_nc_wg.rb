0b79a34f5585cab68b99df1775ed4094fbc1d02f
Ed Overton <e.overton@gmail.com>
2018-05-08 17:54:28 -0400

    working base wg

--- recipes/make_nc_wg.rb
+++ recipes/make_nc_wg.rb
@@ -63,37 +63,12 @@ filelist.each do |_fn, fv|
   end
 end
 
-# create silent install file for WebGUI was packages
-template "#{node['wgisrv']['temp_dir']}/was.xml" do
-  source 'was.xml.erb'
-  not_if { File.exist?("#{node['wgisrv']['was_dir']}/startServer.sh") }
-  mode 0444
-  action :nothing
-end
-
-# Install was + fp + java 8
-execute 'install_was' do
-  command "#{node['wgisrv']['app_dir']}/InstallationManager/eclipse/tools/imcl \
-  input #{node['wgisrv']['temp_dir']}/was.xml \
-  -log #{node['wgisrv']['temp_dir']}/was_log.xml \
-  -acceptlicense"
-  cwd "#{node['wgisrv']['app_dir']}/InstallationManager/eclipse/tools"
-  not_if { File.exist?("#{node['wgisrv']['was_dir']}/startServer.sh") }
-  user node['wgisrv']['nc_act']
-  group node['wgisrv']['nc_grp']
-  umask '022'
-  action :nothing
-end
-
-# remove silent install file
-file "#{node['wgisrv']['temp_dir']}/was.xml" do
-  action :delete
-end
-
 # create silent install file for WebGUI packages
 template "#{node['wgisrv']['temp_dir']}/wg.xml" do
   source 'wg.xml.erb'
   not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/bin/runcgi") }
+  ## must be set because of clear text passwords
+  sensitive true
   mode 0444
 end
 
