91ff803c91e069ab023e708f679ecee34afd6a4a
Ed Overton <e.overton@gmail.com>
2018-05-15 21:26:01 -0400

    copy file

--- recipes/fix_soap.rb
+++ recipes/fix_soap.rb
@@ -1,9 +1,17 @@
+# make a backup copy
+file "#{node['wgisrv']['jaz_dir']}/profile/properties/soap.client.props.orig" do
+  content IO.read("#{node['wgisrv']['jaz_dir']}/profile/properties/soap.client.props")
+  not_if { File.exist?("#{node['wgisrv']['jaz_dir']}/profile/properties/soap.client.props.orig") }
+  action :create
+end
+
 # fix Soap properties to not be prompted for password(s)
 template "#{node['wgisrv']['jaz_dir']}/profile/properties/soap.client.props" do
   source 'soap.client.props.erb'
   user node['wgisrv']['nc_act']
   group node['wgisrv']['nc_grp']
   sensitive true
+  not_if { File.exist?("#{node['wgisrv']['jaz_dir']}/profile/properties/soap.client.props.orig") }
 end
 
 # encode passwords in file
@@ -14,5 +22,6 @@ execute 'encode_soap' do
   cwd "#{node['wgisrv']['jaz_dir']}/profile/bin"
   user node['wgisrv']['nc_act']
   group node['wgisrv']['nc_grp']
+  not_if { File.exist?("#{node['wgisrv']['jaz_dir']}/profile/properties/soap.client.props.orig") }
   action :run
 end
--- recipes/jre_free.rb
+++ recipes/jre_free.rb
@@ -4,17 +4,26 @@ template '/etc/profile.d/java.sh' do
   mode 0755
 end
 
+# make a backup copy
+file "#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init.orig" do
+  content IO.read("#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init")
+  not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init.orig") }
+  action :create
+end
+
 # change server.init
 template "#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init" do
   source 'server.init.erb'
   user node['wgisrv']['nc_act']
   group node['wgisrv']['nc_grp']
   sensitive true
+  not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init.orig") }
 end
 
 ## restart JazzSM/Dash Server
 stop_server 'stop server jre free' do
   only_if { File.exist?(node['wgisrv']['jaz_pid']) }
+  not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init.orig") }
   action :run
 end
 # start wg
@@ -24,5 +33,6 @@ execute 'start_wg_jre' do
   user node['wgisrv']['nc_act']
   group node['wgisrv']['nc_grp']
   not_if { File.exist?(node['wgisrv']['jaz_pid']) }
+  not_if { File.exist?("#{node['wgisrv']['ng_dir']}/omnibus_webgui/etc/server.init.orig") }
   action :run
 end
