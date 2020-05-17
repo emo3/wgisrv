7f95040837bbdcf068bd640cf5f3fea3a466502c
Ed Overton <e.overton@gmail.com>
2020-05-16 22:33:02 -0400

    remove my email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'wgisrv'
 maintainer 'Ed Overton'
-maintainer_email 'infuse.1301@gmail.com'
+maintainer_email 'bogus@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures wgisrv'
 long_description 'Installs/Configures wgisrv'
